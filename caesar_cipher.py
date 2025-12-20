4a98143ca85ab6d764f45b050ec733a661a1dd5f
Genarcarte <bang.bang.rabang@gmail.com>
2025-12-20 15:44:25 +0800

    feat(caesar): step11 add text and shift as parameters and remove text and shift in function body

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,8 +1,8 @@
-def caesar():
+def caesar(text, shift):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
-    shift = 5
+
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
     translation_table = str.maketrans(alphabet, shifted_alphabet)
-    text = "hello world"
+
     encrypted_text = text.translate(translation_table)
     print(encrypted_text)
