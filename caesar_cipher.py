30d4f3594bbaff7e57643d1037273995f25c102f
Genarcarte <bang.bang.rabang@gmail.com>
2025-12-24 16:32:33 +0800

    feat(caesar): step15 include uppercase mapping in translation_table

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -3,7 +3,7 @@ def caesar(text, shift):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
 
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
-    translation_table = str.maketrans(alphabet, shifted_alphabet)
+    translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
 
     return text.translate(translation_table)
 
